d7273bf8e65a2e8db3bb71d6c6f77b2f0336fbdd
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2022-06-05 12:00:45 +0300

    Update DefaultSettings.lua

--- MainModule/Server/Dependencies/DefaultSettings.lua
+++ MainModule/Server/Dependencies/DefaultSettings.lua
@@ -302,35 +302,12 @@ local descs = {};			--// Contains settings descriptions
 	settings.LockMessage = "Not Whitelisted"	-- Message shown to people when they are kicked while the game is :slocked
 	settings.SystemTitle = "System Message"		-- Title to display in :sm and :bc
 
+	settings.OldLogsEnabled = true 		-- If :OldLogs are saved, ie. if commandlogs are saved to oldlogs after game shutdown. Set to false if you wan't to disable.
 	settings.MaxLogs = 5000			-- Maximum logs to save before deleting the oldest
 	settings.Notification = true	-- Whether or not to show the "You're an admin" and "Updated" notifications
 	settings.SongHint = true		-- Display a hint with the current song name and ID when a song is played via :music
 	settings.TopBarShift = false	-- By default hints and notifs will appear from the top edge of the window, this is acheived by offsetting them by -35 into the transparent region where roblox buttons menu/chat/leaderstat buttons are. Set this to true if you don't want hints/notifs to appear in that region.
-
-	settings.Messages = {			-- A list of notification messages to show HeadAdmins and above on join
-	--{ 
-	-- EXAMPLE MESSAGE
-	--
-	--	Title = "Title";
-	--	Message = "This is an example notification";
-	--	Icon = "rbxassetid://7510994359";
-	--	Time = 15;
-	--	OnClick = server.Core.Bytecode([[
-	--		local window = client.UI.Make("Window", {
-	--			Title = "Image of a cute cat displaed in a window when clicked";
-	--			Size = {700,300};
-	--			Icon = "rbxassetid://7510994359";
-	--		})
-
-	--		window:Add("ImageLabel", {
-	--			Image = "rbxassetid://1059543904";
-	--		})
-
-	--		window:Ready()
-	--	]]);
-	--}
-	}
-	
+	settings.Messages = {}			-- A list of notification messages to show HeadAdmins and above on join
 	settings.AutoClean = false		-- Will auto clean workspace of things like hats and tools
 	settings.AutoCleanDelay = 60	-- Time between auto cleans
 	settings.AutoBackup = false 	-- (not recommended) Run a map backup command when the server starts, this is mostly useless as clients cannot modify the server. To restore the map run :restoremap
